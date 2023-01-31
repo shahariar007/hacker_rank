34e8f57df2dd39ad19e75ef5e7060262957a7775
mortuza <mortuza@squaregroup.com>
2023-01-31 17:18:51 +0600

    Restaurant complete

--- Restaurant.java
+++ Restaurant.java
@@ -4,7 +4,10 @@ public class Restaurant {
     private static final String TAG = "Restaurant";
 
     public static void main(String[] args) {
-        System.out.println(calGCD(6,9));
+        int x=2;
+        int y= 2;
+        int xy=x*y;
+        System.out.println(xy/ (int)Math.pow(calGCD(x,y),2));
     }
 
     static int calGCD(int num1, int num2) {
